231aa68c0720ce4f279aac366f2c95c45f5c8fad
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2025-01-07 16:54:54 +0100

    minor

--- ops/bin/certApp.ts
+++ ops/bin/certApp.ts
@@ -3,15 +3,14 @@ import * as cdk from 'aws-cdk-lib';
 
 import { CertificateStack } from '../lib/stacks/certificateStack';
 
-const app = new cdk.App();
-
 const DOMAIN_NAME = 'todo-pro.xyz';
+
+const app = new cdk.App();
 
 new CertificateStack(app, 'CertificateStack', {
   domainName: DOMAIN_NAME,
   synthesizer: new cdk.DefaultStackSynthesizer({
     fileAssetsBucketName: 'rocky-jaiswal-todo-pro-xyz-cdk-assets',
   }),
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  env: { account: '750324395434', region: 'us-east-1' },
+  env: { account: process.env.CDK_ACCOUNT, region: 'us-east-1' },
 });
--- ops/bin/webApp.ts
+++ ops/bin/webApp.ts
@@ -4,25 +4,27 @@ import * as cdk from 'aws-cdk-lib';
 import { DomainStack } from '../lib/stacks/domainStack';
 import { WebStack } from '../lib/stacks/webStack';
 
-const app = new cdk.App();
 const DOMAIN_NAME = 'todo-pro.xyz';
 
+const app = new cdk.App();
+
 const throwError = () => {
-  throw new Error('CERT_ARN must be set');
+  throw new Error('CERT_ARN & CDK_ACCOUNT must be set');
 };
 
 const CERT_ARN = process.env.CERT_ARN ?? throwError();
+const CDK_ACCOUNT = process.env.CDK_ACCOUNT ?? throwError();
 
 const webStack = new WebStack(app, 'WebStack', {
   bucketName: 'rocky-jaiswal-todopro-xyz-web-assets-bucket',
   domainName: DOMAIN_NAME,
   certificateArn: CERT_ARN,
-  env: { account: '750324395434', region: 'eu-central-1' },
+  env: { account: CDK_ACCOUNT, region: 'eu-central-1' },
 });
 
 const domainStack = new DomainStack(app, 'DomainStack', {
   domainName: DOMAIN_NAME,
   distribution: webStack.distribution,
-  env: { account: '750324395434', region: 'eu-central-1' },
+  env: { account: CDK_ACCOUNT, region: 'eu-central-1' },
   crossRegionReferences: true,
 });
